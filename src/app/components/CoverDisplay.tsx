a7527d7d7fc494bfa0954d5457f098f4c027cf6b
Oscar Herrera <a.oscarherrera@gmail.com>
2024-05-09 10:21:07 -0500

    placed coverdisplay component

---
+++ src/app/components/CoverDisplay.tsx
@@ -0,0 +1,21 @@
+
+
+
+
+export default function CoverDisplay () {
+    return (
+        <aside className="bg-zinc-900 rounded-md ml-2 w-[17.5]">
+            <header>
+                cover display
+            </header>
+            <main>
+
+
+            </main>
+
+        </aside>
+
+
+
+    )
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import Navbar from "./components/Navbar"
 import Library from "./components/Library"
 import AlbumShowcase from "./components/AlbumShowcase"
+import CoverDisplay from "./components/CoverDisplay"
 
 export default function Home() {
   return (
     <>
-      <div className="grid grid-cols-[20.6875rem_48.75rem_17.5rem] grid-rows-none p-2 h-[48.5rem]">
+      <div className="grid grid-cols-[20.55rem_48.75rem_17.5rem] grid-rows-none p-2 h-[48.5rem]">
         <div className="grid grid-rows-[7.5rem_46.5rem] h-[40rem]">
         <Navbar/>
         <Library/>
         </div>
         <AlbumShowcase/>
+        <CoverDisplay/>
       </div>
 
     </>
